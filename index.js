9512849de5980dcbd799f56a9f740e1bfff45a5b
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-22 16:09:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,74 @@
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+let store = {drivers: [], passengers: [], trips: []};
+
+class Driver {
+  constructor(name) {
+    this.name = name;
+    this.id = ++driverId;
+    store.drivers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(
+      function (trip) {
+          return trip.driverId === this.id;
+      }.bind(this)
+    );
+  }
+
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = ++passengerId;
+    store.passengers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(
+      function(trip) {
+        return trip.passengerId === this.id;
+      }.bind(this)
+    )
+  }
+
+  drivers() {
+    return this.trips().map(trip =>{
+      return trip.driver();
+    })
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = ++tripId;
+    store.trips.push(this);
+  }
+
+  passenger() {
+     return store.passengers.find(
+       function(passenger) {
+          return passenger.id === this.passengerId;
+       }.bind(this)
+     );
+  }
+
+  driver() {
+    return store.drivers.find(
+      function(driver) {
+          return driver.id === this.driverId;
+      }.bind(this)
+    );
+  }
+
+}
